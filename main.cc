7814656042d53253ae77c019ab4c89bb58e38673
mark1027 <cpatton1027@yahoo.com>
2012-08-24 09:42:58 -0700

    here it is

--- main.cc
+++ main.cc
@@ -5,6 +5,20 @@ using namespace std;
 int main()
 {
 
- 	cout << "How are you" << endl;
+	//FIRST TEST PROJECT: Just silly output
+        int userNumber;
+ 	cout << "Input your favorite number" << endl;
+	cin >> userNumber;
+
+
+	if (userNumber == 7)
+	{
+		cout << "That's my favorite number too!" << end;
+	}
+	else
+	{
+		cout << "Hey, not my favorite number but to each his own";
+	}
+		
 	return 0;
 }
